868488479bd57fb9ec7a1539f39a6bd431b90853
Sadik Moazzem <sadik-moazzem@outlook.com>
2024-12-31 18:45:13 +0000

    Environemnts for lambda

--- mylocalmasjid_api/config.py
+++ mylocalmasjid_api/config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "My Local Masjid API"
     VERSION: str = "4.0.0"
     DESCRIPTION: str = "API for My Local Masjid"
-    ENV: Literal["development", "staging", "production"] = "development"
+    ENV: Literal["development", "staging", "production"] = os.getenv("API_ENV", "development")
     DATABASE_URI: str =  os.getenv("DATABASE_URL", "not_set")
     SECRET_KEY: str =  os.getenv("SECRET_KEY", "not_so_secret")
     ACCESS_TOKEN_EXPIRE: int = 1800 # seconds (30 minutes)
